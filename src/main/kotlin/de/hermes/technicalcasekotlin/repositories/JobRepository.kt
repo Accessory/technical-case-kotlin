4662281a6c74651569ea65a675b7c72469a056be
Bernhard Hermes <b.hermes@brandstreet.de>
2023-11-13 16:53:15 +0100

    Init

---
+++ src/main/kotlin/de/hermes/technicalcasekotlin/repositories/JobRepository.kt
@@ -0,0 +1,7 @@
+package de.hermes.technicalcasekotlin.repositories
+
+import de.hermes.technicalcasekotlin.entities.Job
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface JobRepository : JpaRepository<Job, Long>{
+}
